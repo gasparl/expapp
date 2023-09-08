2d7c8124441bc4c653dc87b7122e62dbb2c88e84
gasparl <lkcsgaspar@gmail.com>
2023-09-08 12:10:07 -0700

    sanitize file name in php

--- php/store_partial.php
+++ php/store_partial.php
@@ -17,16 +17,19 @@ $request = json_decode($postdata);
 # get main data
 $user_data = $request->results_post;
 
+# sanitize the file name
+$file_name = basename($request->fname_post);
+
 # sanity checks (otherwise do not save):
 # (1) file name should end with ".txt" extention
 # (2) the data should contain at least 50 characters
-if (strlen($user_data ?? '') < 50 or substr($request->fname_post ?? '', -4) !== ".txt") {
+if (strlen($user_data ?? '') < 50 or substr($file_name ?? '', -4) !== ".txt") {
     die("Failed");
 }
 
 # set path relative to server root; extend it with the file name [n23]
 $path = $_SERVER['DOCUMENT_ROOT'] . "/../data/template_results_part/";
-$file_name = $path . $user_ip . '_' . $request->fname_post;
+$file_path = $path . $user_ip . '_' . $file_name;
 
 # save the file (if the file already exists, overwrite it [n25])
-file_put_contents($file_name, $user_data);
+file_put_contents($file_path, $user_data);
