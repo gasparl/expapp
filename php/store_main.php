c84131de64b64323b0cfac9be880fe351d5a5a3f
gasparl <lkcsgaspar@gmail.com>
2023-09-08 12:17:36 -0700

    sanitize file name in both phps

--- php/store_main.php
+++ php/store_main.php
@@ -11,8 +11,11 @@ error_reporting(E_ALL);
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
+# sanitize the file name
+$file_name = basename($request->fname_post);
+
 # sanity check: file name should end with ".txt" extention
-if (substr($request->fname_post ?? '', -4) !== ".txt") {
+if (substr($file_name ?? '', -4) !== ".txt") {
     die("Failed: wrong format.");
 }
 
@@ -21,15 +24,15 @@ $user_data = $request->results_post;
 
 # set path relative to server root; extend it with the file name [n23]
 $path = $_SERVER['DOCUMENT_ROOT'] . "/../data/template_results_full/";
-$file_name = $path .  $request->fname_post;
+$file_path = $path .  $file_name;
 
 # save the file (if the file already exists, just append the new data [n26])
-$outcome = file_put_contents($file_name, $user_data, FILE_APPEND | LOCK_EX);
+$outcome = file_put_contents($file_path, $user_data, FILE_APPEND | LOCK_EX);
 
 if ($outcome > 500) {
     // if more than 500 characters were successfully written, return Prolific completion link
     echo "https://app.prolific.co/submissions/complete?cc=EXAMPLE";
-} else if (is_file($file_name) === FALSE) {
+} else if (is_file($file_path) === FALSE) {
     // if the file is not saved, return warning
     echo "Failed to save file! (" . $outcome . "x" . $request->fname_post . ")";
 } else {
